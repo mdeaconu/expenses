13132dabf305bf15613d54469168b6056e2e7a48
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-05-27 15:15:39 +0300

    Adding Navigation & Configuring Navigation
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import AllExpenses from "./screens/AllExpenses";
+import ManageExpense from "./screens/ManageExpense";
+import RecentExpenses from "./screens/RecentExpenses";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+    </BottomTabs.Navigator>
+  );
+};
 
 const App = () => {
-  return <Text>Hello</Text>;
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
